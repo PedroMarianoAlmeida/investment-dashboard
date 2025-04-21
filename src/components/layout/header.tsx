1945289eff8c9fd65dcce3f424ffb0ba5eb6377f
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-21 02:10:33 -0300

    add header (#4)

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="p-4 bg-secondary-foreground">
+      <h1 className="text-primary-foreground text-2xl">Investment Portfolio</h1>
+    </header>
+  );
+};
