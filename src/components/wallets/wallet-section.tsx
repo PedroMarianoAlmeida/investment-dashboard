afed86be6cfb46ec15ff12a9200f4f56cea91291
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-21 00:59:13 -0300

    pass wallet clicked to url params

--- src/components/wallets/wallet-section.tsx
+++ src/components/wallets/wallet-section.tsx
@@ -1,15 +1,5 @@
 import { getWallets } from "@/services/walletService";
-
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-
-import { WalletsDetails } from "@/components/wallets/wallet-detail";
+import { WalletTable } from "@/components/wallets/wallet-table";
 
 export const WalletSection = () => {
   const wallets = getWallets();
@@ -17,25 +7,7 @@ export const WalletSection = () => {
   return (
     <section>
       <h2 className="mb-4 text-2xl font-bold">Wallets</h2>
-
-      <Table className="rounded-lg border border-separate border-spacing-0 p-2">
-        <TableHeader>
-          <TableRow className="grid grid-cols-3 pb-2">
-            <TableHead className="text-muted-foreground h-auto">Name</TableHead>
-            <TableHead className="text-muted-foreground text-right h-auto">
-              Current Amount
-            </TableHead>
-            <TableHead className="text-muted-foreground text-right h-auto">
-              Spent Amount
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {wallets.map((wallet) => (
-            <WalletsDetails walletDetail={wallet} key={wallet.walletName} />
-          ))}
-        </TableBody>
-      </Table>
+      <WalletTable wallets={wallets} />
     </section>
   );
 };
