61cd535edd2325a10fba00dd2c6e30cd9cee1654
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-23 16:26:59 -0300

    add form from docs

--- src/components/assets/add-asset.tsx
+++ src/components/assets/add-asset.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Asset } from "@/types/wallet";
@@ -15,11 +18,43 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
 interface AddAssetProps {
   otherWalletsAssets: Pick<Asset, "name" | "symbol" | "type">[];
 }
 export const AddAsset = ({ otherWalletsAssets }: AddAssetProps) => {
   console.log({ otherWalletsAssets });
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
@@ -30,6 +65,30 @@ export const AddAsset = ({ otherWalletsAssets }: AddAssetProps) => {
           <AlertDialogTitle>Add asset</AlertDialogTitle>
           <AlertDialogDescription>
             Add a new asset into Wallet
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
