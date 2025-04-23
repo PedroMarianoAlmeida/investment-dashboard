6b54c750fe6f3cb1dd2419139baa690c522d4d88
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-23 01:28:31 -0300

    get only data (no collection) for user

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/config/authConfig";
 
+import { getWalletFromUser } from "@/services/dbService";
+
 export default async function DashboardPage() {
   const session = await getServerSession(authOptions);
-  console.log("from dashboard", { session });
+  const data = await getWalletFromUser();
+  console.log({ data });
   return <div>Dashboard</div>;
 }
--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -1,11 +1,32 @@
+import { getServerSession } from "next-auth/next";
 import { doc, getDoc } from "firebase/firestore";
 
 import { database } from "@/config/databaseConfig";
+import { authOptions } from "@/config/authConfig";
 import { asyncWrapper } from "@/helpers/asyncWrapper";
 
 export const getTest = () => {
   return asyncWrapper(async () => {
     const docRef = doc(database, "test", "Uo6tGAeMv3OdwPL5j8PS");
+    const docSnap = await getDoc(docRef);
+
+    if (!docSnap.exists()) throw new Error("Document not founded");
+    const data = docSnap.data();
+
+    return { data };
+  });
+};
+
+export const getWalletFromUser = async () => {
+  return asyncWrapper(async () => {
+    const session = await getServerSession(authOptions);
+    if (!session) throw new Error("No session");
+
+    const {
+      user: { id },
+    } = session;
+
+    const docRef = doc(database, `users`, id);
     const docSnap = await getDoc(docRef);
 
     if (!docSnap.exists()) throw new Error("Document not founded");
