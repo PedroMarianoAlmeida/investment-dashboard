915c19288ff0e708e110af7caa3f29b1775bb0fe
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-22 21:25:04 -0300

    add auth

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+
 export const Header = () => {
+  const { data } = useSession();
+  console.log({ data });
+
   return (
     <header className="p-4 bg-secondary-foreground">
       <h1 className="text-primary-foreground text-2xl">Investment Portfolio</h1>
+      <Button onClick={() => signIn()}>Sign In</Button>
+      <Button onClick={() => signOut()}>Sig Out</Button>
     </header>
   );
 };
