dd384bb568fb7651ae9fa5fee5ecc563e032f671
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-24 08:45:55 -0300

    fix header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,13 +3,16 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="px-4 py-1 bg-secondary-foreground flex justify-between items-center">
-      <Link href="/">
-        <h1 className="text-primary-foreground text-2xl">
-          Investment Portfolio
-        </h1>
-      </Link>
-      <AuthMenu />
-    </header>
+    <>
+      <div className="h-14 w-full" />
+      <header className="px-4 bg-secondary-foreground flex justify-between items-center h-14 fixed top-0 w-full z-10">
+        <Link href="/">
+          <h1 className="text-primary-foreground text-2xl">
+            Investment Portfolio
+          </h1>
+        </Link>
+        <AuthMenu />
+      </header>
+    </>
   );
 };
